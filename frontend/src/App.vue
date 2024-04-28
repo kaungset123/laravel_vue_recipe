c91e4f452443563234405a6a902cc53f530b7823
kaungset123 <kaungsetnaing2110@gmail.com>
2024-04-28 15:20:08 +0630

    frontend axios setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <h1 class="bg-sky-700">Hello</h1>
+  <ul v-for="category in categories" :key="category.id">
+    <li>{{ category.name }}</li>
+  </ul>
 </template>
 
+<script>
+  export default {
+    data(){
+      return {
+        categories: []
+      }
+    },
+    methods: {
+      async getCategories(){
+        let res = await this.$axios.get('/api/categories');
+        this.categories = res.data
+      }
+    },
+    mounted(){
+      this.getCategories()
+    }
+  }
+</script>
 <style scoped>
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import './index.css'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = "http://localhost:8000"
+
+const app = createApp(App)
+
+app.config.globalProperties.$axios = axios; // globaling the axios to be use the url without http://localhost:8000 and without importing axios everywhere
+app.mount('#app')
